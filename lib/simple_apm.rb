4b230f4a62441106d305211deafbd7672e895005
yuanyin.xia <yuanyin.xia@jikabao.com>
2018-06-01 16:38:48 +0800

    报错rescue到log下

--- lib/simple_apm.rb
+++ lib/simple_apm.rb
@@ -1,7 +1,7 @@
 require "simple_apm/setting"
 require "simple_apm/redis"
 require "simple_apm/engine"
-
+require 'callsite'
 module SimpleApm
   ActiveSupport::Notifications.subscribe('process_action.action_controller') do |name, started, finished, unique_id, payload|
     request_id = Thread.current['action_dispatch.request_id']
@@ -33,8 +33,8 @@ module SimpleApm
         SimpleApm::Sql.delete_by_request_id(request_id)
       end
     end
-  # rescue => e
-  #   Logger.new("#{Rails.root}/log/simple_apm.log").info e.inspect
+  rescue => e
+    Logger.new("#{Rails.root}/log/simple_apm.log").info e.backtrace
   end
 
   ActiveSupport::Notifications.subscribe 'sql.active_record' do |name, started, finished, unique_id, payload|
@@ -42,7 +42,7 @@ module SimpleApm
     if request_id.present?
       dev_caller = caller.detect { |c| c.include? Rails.root.to_s }
       if dev_caller
-        c = Callsite.parse(dev_caller)
+        c = ::Callsite.parse(dev_caller)
         payload.merge!(:line => c.line, :filename => c.filename.to_s.gsub(Rails.root.to_s,''), :method => c.method)
       end
       # ActiveRecord::Relation::QueryAttribute
@@ -59,7 +59,7 @@ module SimpleApm
       }.with_indifferent_access
       SimpleApm::Sql.create request_id, info
     end
-  # rescue => e
-  #   Logger.new("#{Rails.root}/log/simple_apm.log").info e.inspect
+  rescue => e
+    Logger.new("#{Rails.root}/log/simple_apm.log").info e.backtrace
   end
 end
