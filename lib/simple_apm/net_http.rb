75d17752f572c85cc2a81c6f9e190d6e012eda90
yuanyin.xia <yuanyin.xia@jikabao.com>
2018-08-21 14:56:09 +0800

    修复HTTParty一次调用会出现两次log的问题(不记录connect耗时)

--- lib/simple_apm/net_http.rb
+++ lib/simple_apm/net_http.rb
@@ -3,12 +3,24 @@ module SimpleApm
     class << self
       def install
         Net::HTTP.class_eval do
-          alias orig_request request unless method_defined?(:orig_request)
+          alias orig_request_apm request unless method_defined?(:orig_request)
 
           def request(req, body = nil, &block)
-            url = "http://#{@address}:#{@port}#{req.path}"
-            ActiveSupport::Notifications.instrument "net_http.request", url: url, host: @address, path: req.path do
-              @response = orig_request(req, body, &block)
+            url = if @port == '80'
+                    "http://#{@address}#{req.path}"
+                  elsif @port == '443'
+                    "https://#{@address}#{req.path}"
+                  else
+                    "http://#{@address}:#{@port}#{req.path}"
+                  end
+            # 会调用两次(HTTParty)
+            if started?
+              ActiveSupport::Notifications.instrument "net_http.request", url: url, host: @address, path: req.path do
+                @response = orig_request_apm(req, body, &block)
+              end
+            else
+              # 去connect
+              @response = orig_request_apm(req, body, &block)
             end
             @response
           end
